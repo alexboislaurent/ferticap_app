209ed4411877c95f908c76a15898d39ce3cd42a9
alexboislaurent <93996590+alexboislaurent@users.noreply.github.com>
2026-08-06 17:09:40 +0200

    Update scores.py

--- plots/scores.py
+++ plots/scores.py
@@ -1,1 +1,55 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
+
+def create_score_global(score, lissage):
+
+    if lissage > 1:
+        score = (
+            score
+            .rolling(
+                window=lissage,
+                min_periods=1
+            )
+            .mean()
+        )
+
+
+    fig, ax = plt.subplots(
+        figsize=(12,5)
+    )
+
+
+    ax.plot(
+        score.index,
+        score.values,
+        marker="o"
+    )
+
+
+    ax.grid(True)
+
+    ax.set_title(
+        "Score moyen global"
+    )
+
+    ax.set_ylabel(
+        "Score"
+    )
+
+
+    ax.xaxis.set_major_formatter(
+        mdates.DateFormatter("%d/%m/%y")
+    )
+
+    ax.xaxis.set_major_locator(
+        mdates.AutoDateLocator()
+    )
+
+
+    fig.autofmt_xdate(
+        rotation=45
+    )
+
+
+    return fig
