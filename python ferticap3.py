6bf22e5712708b543d666ad6bcf4d04c0f35a91c
alexboislaurent <93996590+alexboislaurent@users.noreply.github.com>
2026-08-06 16:59:05 +0200

    Update python ferticap3.py

--- python ferticap3.py
+++ python ferticap3.py
@@ -27,29 +27,12 @@ worksheet = load_google_sheet()
 # =========================
 # DATA
 # =========================
+from data.cleaning import clean_data
+
 
 all_values = worksheet.get_all_values()
 
-headers = all_values[1]
-rows = all_values[2:]
-
-df = pd.DataFrame(rows, columns=headers)
-# Nettoyage des noms de colonnes (espaces cachés)
-df.columns = (
-    df.columns
-    .astype(str)
-    .str.strip()
-)
-
-df["Date"] = pd.to_datetime(df["Date"], errors="coerce", dayfirst=True)
-df["Comportement"] = pd.to_numeric(df["Comportement"], errors="coerce")
-
-df["Score"] = pd.to_numeric(
-    df["Score"].astype(str).str.replace(",", "."),
-    errors="coerce"
-)
-
-df["Succes"] = df["Comportement"].isin([2, 3, 4]).astype(int)
+df = clean_data(all_values)
 # =========================
 # VARIABLE SUIVI DES SAUTS
 # =========================
