bf897481c54ae3dc3ab80c9c2ea0c38a8200edf6
alexboislaurent <93996590+alexboislaurent@users.noreply.github.com>
2026-08-06 17:06:59 +0200

    Update heatmap.py

--- plots/heatmap.py
+++ plots/heatmap.py
@@ -1,1 +1,32 @@
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def create_heatmap(data):
+
+    fig, ax = plt.subplots(
+        figsize=(16, 6)
+    )
+
+    sns.heatmap(
+        data,
+        cmap="RdYlGn",
+        cbar=False,
+        ax=ax,
+        linewidths=0.5,
+        linecolor="black"
+    )
+
+    ax.set_yticklabels(
+        ax.get_yticklabels(),
+        rotation=0
+    )
+
+    ax.set_xticklabels(
+        ax.get_xticklabels(),
+        rotation=45,
+        ha="right"
+    )
+
+    return fig
