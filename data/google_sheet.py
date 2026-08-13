10c21810682e8fa4d30223301d553b1d2adbe484
alexboislaurent <93996590+alexboislaurent@users.noreply.github.com>
2026-08-13 15:17:31 +0200

    Update google_sheet.py

--- data/google_sheet.py
+++ data/google_sheet.py
@@ -26,3 +26,27 @@ def load_google_sheet():
     )
 
     return worksheet
+
+def load_rations_sheet():
+
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_service_account"],
+        scopes=scope
+    )
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open_by_key(
+        "178LJjutfRAO0cvw4aCJ2RClOuMWv1-I94X0FL8Lcs0c"
+    )
+
+    worksheet = sheet.worksheet(
+        "Rations"
+    )
+
+    return worksheet
